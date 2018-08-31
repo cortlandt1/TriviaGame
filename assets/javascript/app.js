09668605bcbe176e632786b5ed0177f78f79026b
Gordo <cortland.taylor1@gmail.com>
2018-08-31 12:03:36 -0700

    Final changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,20 @@
-//Questions -- 
-//What was the first movie released in the Marvel Cinematic Universe?
-// -The Incredible Hulk(c) -Iron Man -Captain America The First Avenger -Thor
 
-//Which Infinity stone was found inside Loki's septre?
-// -The Time stone -The Space stone -The Mind stone(c) -The Reality stone
-
-//Captain America's shield is made out of which material?
-// -Adamantium -Titanium -Vibranium(c) -Steel alloy
-
-//Black Widow's first apperance was in which film?
-// -Iron Man -The Avengers -Iron Man 2(c) -The Black Widow
-
+//Declaring variables
 let amountCorrect = 0;          
 let wrongAnswers = 0
 let unansweredQuestions = 0
 
+//Appends score and timer data
+$('#correctAnswer').html(amountCorrect)
+$('.time').html('00:30')
+$('#correctAnswer').html(`Correct Answers: ${amountCorrect}`)
+$('#incorrectAnswer').html(`Incorrect Answers: ${wrongAnswers}`)
 
-$('#title').html(amountCorrect)
-
-function handleClick()
+//Loop function for determining correct answers
+function submitAnswers()
 {         
+    // event.preventDefault();
+    clearInterval(gameTimer)
     for(var i = 1; i <= 45; i++) {
         var radios = document.getElementsByName('question'+i);
         for(var j = 0; j < radios.length; j++) {
@@ -31,51 +26,47 @@ function handleClick()
                 wrongAnswers++;
             }
         }
-    }           
-    $('#title').html(amountCorrect)
-    alert("Wrong Responses:" + wrongAnswers)        
-    alert("Correct Responses: " + amountCorrect);
+    }
+    $('#correctAnswer').html(`Correct Answers: ${amountCorrect}`)
+    $('#incorrectAnswer').html(`Incorrect Answers: ${wrongAnswers}`)
 }
 
-    // var marvelQuestions = [{
-    //     question:"What was the first movie released in the Marvel Cinematic Universe?",
-    //     choices: ["The Incredible Hulk", "Iron Man", "Captain America The First Avenger", "Thor"],
-    //     correctAnswer: 0 
-    // }, {
-    //                 question:"Which Infinity stone was found inside Loki's septre?",
-    //                 choices: ["The Time stone", "The Space stone", "The Mind stone", "The Reality stone"],
-    //                 correctAnswer: 2 
-    //             }, {
-    //                 question:"Captain America's shield is made out of which material?",
-    //                 choices: ["Adamantium", "Titanium", "Vibranium", "Wood"],
-    //                 correctAnswer: 2 
-    //             }, {
-    //                 question:"Black Widow's first apperance was in which film?",
-    //                 choices: ["Iron Man", "The Avengers", "Iron Man 2", "The Black Widow"],
-    //                 correctAnswer: 2 
-    //             },
-                
-    //         ];
-// $('#r4').on('click', function (){
-    //     let questionOne = $("input:radio[name=question1]").val()
-    //     if (questionOne == 'hulk') {
-        //         correctAnswers++
-        //         alert('You bitch')
-        //     } else {
-            //         alert('bruh')
-            //     }
-            // })
-            // window.addEventListener("DOMContentLoaded", function () {
-                //     var form = document.getElementById("form-id");
-                
-                // document.getElementById("submit").addEventListener("click", function () {
-        //   form.submit();
-  
-        //     })
-        // })
-        
-        // var selectedOption = $("input:radio[name=question1]:checked").val()
-        
-        //     if (document.getElementById('r4').checked) {
-            //         let rate_value = document.getElementById('r4').value
-            //   };
+//Time Conversion function
+    function timeConversion (t) {
+        var minutes = Math.floor(t / 60)
+        var seconds = t - (minutes * 60)
+
+        if (seconds < 10) {
+          seconds = '0' + seconds
+        }
+
+        if (minutes === 0) {
+          minutes = '00'
+        } else if (minutes < 10) {
+          minutes = '0' + minutes
+        }
+
+        return minutes + ':' + seconds
+    }
+
+//Sets timer
+    let time = 30
+    let gameTimer = setInterval(function () {
+        time--
+        timeConversion(time)
+        if(time > 0) {
+            $('.time').html(timeConversion(time))
+        } else {
+            $('.time').html('00:00')
+            submitAnswers()
+            }
+        }, 1000)
+
+
+
+
+
+
+
+
+
